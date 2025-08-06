ab5eb33477de786b4ff779a26932c54dbe40e015
Lapidis2 <hitlapidis2@gmail.com>
2025-08-06 11:54:22 +0200

    added folder arrangement

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Contact=()=>{
+	return (
+		<div>
+			<h1>Welcome to the contact Page</h1>
+			<p>This is the contact page of our application.</p>
+		</div>
+	);
+}
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar'; 
+const Home = () => {
+	return (
+		<div >
+			<NavBar />
+			<h1>Welcome to the Home Page</h1>
+			<p>This is the home page of our application.</p>
+		</div>
+	);
+}
+export default Home;
